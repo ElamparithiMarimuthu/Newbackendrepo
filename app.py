cb3f882bb96d79fb8cbbb1a3e7e8af736f465367
ElamparithiMarimuthu <parithie713@gmail.com>
2024-12-01 10:52:41 +0530

    which is simple flask app

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/Elamparithi", methods=["GET"])
+def profile():
+    skills = ["DevOps", "Python", "Aws", "Sql", "Kubernetes"]
+    like = ["Fitness", "Cricket", "Chess"]
+    data = {}
+
+    # Adding skills to data
+    for i in range(len(skills)):
+        name = f"skill{i + 1}"
+        data[name] = skills[i]
+
+    # Adding likes to data
+    sports_likes = [f"He likes this sport: {i + 1}. {like[i]}" for i in range(len(like))]
+    data["sports_likes"] = sports_likes
+
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
